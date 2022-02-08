c156ed9da086c8f6978a46ee3586bfd9147f21d9
DarDog <samediacc666@gmial.com>
2022-02-09 00:50:20 +1100

    Feat: Add Blog component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from "../Layout/Layout";
+import Blog from "../Blog/Blog";
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout/>}>
-
+        <Route index element={<Blog />} />
       </Route>
     </Routes>
   );

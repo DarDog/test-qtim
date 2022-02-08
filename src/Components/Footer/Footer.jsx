e22f517ca81ebd5138132a2cef1a2585089f89cc
DarDog <samediacc666@gmial.com>
2022-02-09 00:45:48 +1100

    Feat: Add layout for Footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <></>
+    <footer className='site-footer'>
+      <div className='container'>
+        <div className='site-info'>
+          <h2 className='site-title footer-title'>QTIM test</h2>
+          &#169; Your Website.com. - Design & Code by
+          <a href="https://www.wowthemes.net/" target='_blank'>WowThemes</a>
+        </div>
+      </div>
+    </footer>
   );
 };
 

6111139d783dc17dd17827f2d42121b59ae88f43
DarDog <samediacc666@gmial.com>
2022-02-09 03:08:15 +1100

    Feat: Add Post component

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from "../Layout/Layout";
 import Blog from "../Blog/Blog";
 import { PostsProvider } from "../../context/postsContext";
+import Post from "../Post/Post";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={ <Layout/> }>
           <Route index element={ <Blog/> }/>
+          <Route path='post/:id' element={ <Post/> }/>
         </Route>
       </Routes>
     </PostsProvider>
---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+  return (
+    <>
+      <div className='content-area column two-thirds single-portfolio'></div>
+      <div className='column third'></div>
+    </>
+  );
+};
+
+export default Post;

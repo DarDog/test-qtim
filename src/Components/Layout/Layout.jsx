fda5d346174a58ed7a4294a4c8b46315576ed300
DarDog <samediacc666@gmial.com>
2022-02-09 00:48:48 +1100

    Feat: Add layout for Layout component

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 const Layout = () => {
   return (
-    <div className='container'>
-      <Header />
-    </div>
+    <>
+      <div className='container'>
+        <Header/>
+        <div className='site-content'>
+          <div className='content-area column full'>
+            <main className='site-main'>
+              { <Outlet /> }
+            </main>
+          </div>
+        </div>
+      </div>
+      <Footer/>
+    </>
   );
 };
 

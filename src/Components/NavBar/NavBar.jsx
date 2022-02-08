143b7beb316d0161848dbdbb7eab4f7d1708e37a
DarDog <samediacc666@gmial.com>
2022-02-09 00:13:41 +1100

    Feat: Add Layout fot NavBar component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const MavBar = () => {
+const NavBar = () => {
   return (
-    <>
-    </>
+    <nav className='main-navigation'>
+      <div className='menu-menu-1-container'>
+        <ul className='menu nav-menu'>
+          <li>
+            <Link to='/'>home</Link>
+          </li>
+          <li>
+            <Link to='/'>about</Link>
+          </li>
+          <li>
+            <Link to='/'>shop</Link>
+          </li>
+          <li>
+            <Link to='/'>blog</Link>
+          </li>
+          <li>
+            <Link to='/'>elements</Link>
+          </li>
+          <li>
+            <Link to='/'>pages</Link>
+          </li>
+          <li>
+            <Link to='/'>contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
-export default MavBar;
+export default NavBar;

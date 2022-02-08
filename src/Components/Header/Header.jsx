4b718f4429db3c1c0f5b7702d8de1ad9fb637d68
DarDog <samediacc666@gmial.com>
2022-02-09 00:13:51 +1100

    Feat: Add Layout fot Header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import MavBar from "../NavBar/NavBar";
 
 const Header = () => {
   return (
-    <></>
+    <header className='site-header'>
+      <div className='site-branding'>
+        <h1 className='site-title'>
+          <a href="https://qtim.pro/" target='_blank'>QTIM test</a>
+        </h1>
+        <h2 className='site-description'>Тестовое задание для QTIM</h2>
+      </div>
+      <MavBar />
+    </header>
   );
 };
 
